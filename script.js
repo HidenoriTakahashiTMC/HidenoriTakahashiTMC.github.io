4a35d14cad5e1c1c10c41dc39ad3ab1d45c62156
HidenoriTakahashiTMC <137020785+HidenoriTakahashiTMC@users.noreply.github.com>
2023-08-02 04:13:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -14,21 +14,21 @@ function leave(x) {
 
 //判定を付けていく
 //イベントリスナー
-const target = document.getElementById("gtext1");
-console.log(target)
-target[0].addEventListener("click",result1);
+// const target = document.getElementById("gtext1");
+// console.log(target)
+// target[0].addEventListener("click",result1);
 
 
-// const targetRiver = document.getElementById("makeImg2");
+// // const targetRiver = document.getElementById("makeImg2");
 
-//関数を設定
-function result1(x){
-    // let numbersOfGators = document.getElementById('number').value;  //入力値をゲット
-    let result =target.textContent;                         //空変数を用意
-    // console.log(numbersOfGators)
-    // for(let i = 1;i<=numbersOfGators;i++){      //入力値だけループして繰り返す
-    //     result+="🐊";           //[windows] +[.]　絵文字は文字として処理できる
-    // }
-    target.textContent=result + "A";
-    // console.log(result)
-}
+// //関数を設定
+// function result1(x){
+//     // let numbersOfGators = document.getElementById('number').value;  //入力値をゲット
+//     let result =target.textContent;                         //空変数を用意
+//     // console.log(numbersOfGators)
+//     // for(let i = 1;i<=numbersOfGators;i++){      //入力値だけループして繰り返す
+//     //     result+="🐊";           //[windows] +[.]　絵文字は文字として処理できる
+//     // }
+//     target.textContent=result + "A";
+//     // console.log(result)
+// }
